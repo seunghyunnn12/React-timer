198032ec888f5cfdf6b90e689161adda11f2ad97
seunghyunnn12 <shtmdgus16@naver.com>
2025-03-27 10:36:49 +0900

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import Timer from './components/Timer'
+
+function App() {
+
+  return (
+    <div className='app-container'>
+      <h1>Timer App</h1>
+      <Timer/>
+    </div>
+  )
+}
+
+export default App
